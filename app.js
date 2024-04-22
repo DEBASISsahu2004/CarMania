37218389f81b9cb1657da01f5b33404cdf0a4aad
Debasis <sahurocky524@gmail.com>
2024-04-22 10:56:13 +0530

    added online mongodb

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require ("express");
 const mongoose = require ("mongoose");  
 const app = express();
-mongoose.connect("mongodb://localhost:27017/carmania");
+mongoose.connect("mongodb+srv://debasis:mongodb@cluster0.pqqw5uj.mongodb.net/carmania");
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
